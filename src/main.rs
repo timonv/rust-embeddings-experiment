b04d926c54b92b93c1b911b266f19d5bac5e1d0d
Timon Vonk <timonv@gmail.com>
2023-04-20 10:07:12 +0200

    refactor(main.rs): use .into() method to convert array to HashMap test(main.rs): add unit test for average_word_embedding function

--- src/main.rs
+++ src/main.rs
@@ -37,12 +37,37 @@ fn main() {
         ("banana", [3.0, 4.0, 5.0]),
         ("grape", [4.0, 5.0, 6.0]),
     ]
-    .iter()
-    .cloned()
-    .collect();
+    .into();
 
     // Compute the average word embedding for the bag of words.
     let avg_embedding = average_word_embedding(&bag_of_words, &word_embeddings);
 
     println!("The average word embedding is: {:?}", avg_embedding);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn float_eq(a: f64, b: f64, eps: f64) -> bool {
+        (a - b).abs() < eps
+    }
+
+    #[test]
+    fn test_average_word_embedding() {
+        let bag_of_words = vec!["apple", "orange", "banana", "apple", "grape"];
+        let word_embeddings: HashMap<&str, [f64; 3]> = [
+            ("apple", [1.0, 2.0, 3.0]),
+            ("orange", [2.0, 3.0, 4.0]),
+            ("banana", [3.0, 4.0, 5.0]),
+            ("grape", [4.0, 5.0, 6.0]),
+        ]
+        .into();
+        let avg_embedding = average_word_embedding(&bag_of_words, &word_embeddings);
+        let expected_embedding = [2.2, 3.2, 4.2];
+        let eps = 1e-9;
+        assert!(float_eq(avg_embedding[0], expected_embedding[0], eps));
+        assert!(float_eq(avg_embedding[1], expected_embedding[1], eps));
+        assert!(float_eq(avg_embedding[2], expected_embedding[2], eps));
+    }
 }
