b35bc79ff1096b9c5e903213e9f2c3bafb0538f4
Timon Vonk <timonv@gmail.com>
2023-04-20 10:02:55 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,48 @@
+use std::collections::HashMap;
+
+// Function to compute the average word embedding for a bag of words.
+fn average_word_embedding(
+    bag_of_words: &[&str],
+    word_embeddings: &HashMap<&str, [f64; 3]>,
+) -> [f64; 3] {
+    // Initialize total_embedding.
+    let mut total_embedding = [0.0; 3];
+
+    // Sum the embeddings of the words in the bag.
+    bag_of_words
+        .iter()
+        .filter_map(|word| word_embeddings.get(word))
+        .for_each(|embedding| {
+            total_embedding
+                .iter_mut()
+                .zip(embedding.iter())
+                .for_each(|(total, value)| *total += *value);
+        });
+
+    // Compute the average word embedding.
+    let bag_size = bag_of_words.len() as f64;
+    [
+        total_embedding[0] / bag_size,
+        total_embedding[1] / bag_size,
+        total_embedding[2] / bag_size,
+    ]
+}
+
+fn main() {
+    let bag_of_words = vec!["apple", "orange", "banana", "apple", "grape"];
+
+    let word_embeddings: HashMap<&str, [f64; 3]> = [
+        ("apple", [1.0, 2.0, 3.0]),
+        ("orange", [2.0, 3.0, 4.0]),
+        ("banana", [3.0, 4.0, 5.0]),
+        ("grape", [4.0, 5.0, 6.0]),
+    ]
+    .iter()
+    .cloned()
+    .collect();
+
+    // Compute the average word embedding for the bag of words.
+    let avg_embedding = average_word_embedding(&bag_of_words, &word_embeddings);
+
+    println!("The average word embedding is: {:?}", avg_embedding);
+}
